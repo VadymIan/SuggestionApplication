04d2b1dbd279623949f53c9279dc9e78cf87c152
Vadym_Ianchenko <ia_vadim@ukr.net>
2022-08-16 14:27:42 +0300

    add tests

---
+++ SuggestionApiTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace SuggestionApiTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4 + 1, 1 + 4);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(4 + 1, 2 + 3);
+        }
+    }
+}
